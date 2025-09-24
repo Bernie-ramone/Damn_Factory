d4f76b8a8c63396c02f670ca67e59f79b0342fac
Bernie-ramone <bernie_ramone@outlook.com>
2025-09-24 16:52:43 -0600

    add CORS configuration

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 1. Create API for Damn Factory
 1.1 Modify configuration file   - "launchBrowser": false,
-1.2 
+
+2. Service Extensions
+2.1 Add Service Extensions folder
+2.2 Add ServiceExtension.cs and create CorsConfiguration method
+2.3 
---
+++ src/Damn_Factory.API/Extensions/ServiceExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace Damn_Factory.API.Extensions
+{
+    public static class ServiceExtensions
+    {
+        // Add extension methods for IServiceCollection here for cors
+
+        public static void ConfigureCors(this IServiceCollection services) =>
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                    builder.AllowAnyOrigin()
+                           .AllowAnyMethod()
+                           .AllowAnyHeader());
+            });
+
+    }
+}
